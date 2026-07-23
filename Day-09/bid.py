f044a1c0afe4a56ba650f39c3be6e5e75c4a08ee
Hanan Nasr Zaher <onea3259@gmail.com>
2026-07-23 22:48:46 +0300

    Update bid.py

--- Day-09/bid.py
+++ Day-09/bid.py
@@ -1,11 +1,3 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
-import art
-print(art.logo)
-
-
 def the_big_bidder(bidding_dictionary):
     winner=""
     high_bid=0
